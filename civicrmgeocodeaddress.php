d565276f7cfbf15be0b56c5dd3f3089fdf1a4142
unknown <mathavan@vedaconsulting.co.uk>
2017-05-25 12:17:26 +0100

    #3549 - Geocode changes for already processed records

--- civicrmgeocodeaddress.php
+++ civicrmgeocodeaddress.php
@@ -144,3 +144,41 @@ function civicrmgeocodeaddress_civicrm_navigationMenu( &$params ) {
     );
   }
 }
+
+/**
+ * Implementation of hook_civicrm_post
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_post
+ * To Unset the old address result which sitting on geocode result table. 
+ * 
+ * Geocode update works fine when change to new post code.. But its not picking up if we put back the old post code. because API skips those record since those already sitting on result table. 
+ */
+function civicrmgeocodeaddress_civicrm_post($op, $objectName, $objectId, &$objectRef) {
+  if ($op == 'edit' && $objectName == 'Address') {
+    $newPostalCode = $objectRef->postal_code;
+
+    //check new postal code already exist in geocode result table. if it is, then remove the old record and let API to Update geocode location for new postal address by schedule job.
+    $sqlParams = array(
+      1 => array( $objectId, 'Integer' ),
+      2 => array( strtoupper($newPostalCode), 'String' ),
+    );
+    $sql = "SELECT id FROM civicrm_address_geocoding_result WHERE address_id = %1 AND UPPER(postal_code) = %2";
+
+    //Some cases may no changes in Postal code but geocode is not matched since it might old record need to do data fix?. then reset it. Let API to pick up and update the geocode.
+    if ($objectRef->geo_code_1) {
+      $sql .= " AND geocode_1 != %3";
+      $sqlParams[3] = array( $objectRef->geo_code_1, 'String' );
+    }    
+    if ($objectRef->geo_code_2) {
+      $sql .= " AND geocode_2 != %4";
+      $sqlParams[4] = array( $objectRef->geo_code_2, 'String' );
+    }
+
+    $newPostalCodeExist = CRM_Core_DAO::singleValueQuery($sql, $sqlParams);
+    //Unset the existing postal record from geocode result table
+    if ($newPostalCodeExist) {
+      $sql = "DELETE FROM civicrm_address_geocoding_result WHERE id = %1";
+      CRM_Core_DAO::executeQuery($sql, array(1=>array($newPostalCodeExist , 'Integer')));
+    }
+  }
+}
